84e757850842300703b22ea8af985000f9b63ab9
Your kaminosekai54 <culpin.alexis@laposte.net>
2022-12-16 16:57:56 +0100

    commenting  the code, adding function description

--- getInatData.py
+++ getInatData.py
@@ -4,12 +4,18 @@ import inspect
 from pyinaturalist_convert import *
 import os
 
-
-def getData():
+# getData,
+# this function will use the pinaturalist package to 
+# scrap the inaturalist website and get data,
+# it will concatenate each page result to one dataset
+# @param,
+# @keyWord, the keyword / phrase to search
+def getData(keyWord):
     dfList = []
     try:
         for page in range(1, 100):
-            tmp = get_observations(q="Harmonia Axyridis", d1 = "01/01/2018", d2 = "30/11/2022", lat=53.94220, lng=-3.96803, radius=511, per_page=400, page = page)
+            # tmp = get_observations(q="Harmonia Axyridis", d1 = "01/01/2018", d2 = "30/11/2022", lat=53.94220, lng=-3.96803, radius=511, per_page=400, page = page)
+            tmp = get_observations(q=keyWord, d1 = "01/01/2018", d2 = "30/11/2022", lat=53.94220, lng=-3.96803, radius=511, per_page=400, page = page)
             dfList.append(to_dataframe(tmp))
     except Exception as e: 
         df = pd.concat(dfList)
@@ -21,7 +27,11 @@ def getData():
 
     
     
-
+# filter Data,
+# this function filter the dataset obtain in  the getData function
+# to keep only columns of interest,
+# it also convert the location column to latitude and longitude
+# finally, it standardise the column name of our two dataset
 def filterData():
     inat_df = pd.read_csv("inat_data.csv")
     inat_df = inat_df [["quality_grade", "observed_on_details.date", "species_guess", "public_positional_accuracy", "taxon.name", "geojson.coordinates", "owners_identification_from_vision", "identifications_count", "num_identification_disagreements", "location", "place_guess"]]
@@ -45,28 +55,25 @@ def filterData():
     inat_df.to_csv("inat_data_filtered.csv", sep=",", index=False)
 
 
-
+# getGrid,
+# this function is a try of chunking a big density dataset to a usable for us
 def getGrid():
     latList, longList, densityList = [], [], []
-    # df = pd.read_csv("population_gbr_2019-07-01.csv")
-    # print(df.columns)
     for i,chunk in enumerate(pd.read_csv("population_gbr_2019-07-01.csv", chunksize=10000)):
         latList.append(round(chunk.tail(1).Lat.values[0], 3))
         longList.append(round(chunk.tail(1).Lon.values[0], 3))
         densityList.append(chunk.Population.sum())
-        # print(chunk.tail(1))
-        # print(chunk.tail(1).Lat)
-    # chunk.to_csv('../tmp/split_csv_pandas/chunk{}.csv'.format(i), index=False)
     
     data = {"latitude": latList, "longitude":longList, "density":densityList}
     df = pd.DataFrame.from_dict(data)
     df.to_csv("density_try.csv", sep=",", index=False)
     
-    
+    # main,
+    # main function to run our data collection and cleaning
 def main():
     if not os.path.isfile("inat_data.csv"): getData()
-    # filterData()
-    getGrid()
+    filterData()
+    # getGrid()
     
     
 if __name__ == '__main__':
